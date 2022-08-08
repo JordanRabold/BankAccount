e128c8e226ebc9bf8c1a93c015a3b7d3419569d0
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-08 07:17:13 -0700

    Starting withdraw method implementation and tests

--- BankAccount/Account.cs
+++ BankAccount/Account.cs
@@ -37,17 +37,23 @@ namespace BankAccount
         /// <returns>The new balance after the deposit</returns>
         public double Deposit(double amt)
         {
+            if(amt <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"The {nameof(amt)} must be more than 0");
+            }
             Balance += amt;
             return Balance;
         }
 
         /// <summary>
-        /// Withdraws a specified amount of money from the balance
+        /// Withdraws a specified amount of money from the balance and returns the updates balance
         /// </summary>
         /// <param name="amt">The positive amount of money to be taken from the balance</param>
-        public void Withdrawl(double amt)
+        /// <returns>Returns updated balance after withdrawal</returns>
+        public double Withdraw(double amt)
         {
-            throw new NotImplementedException();
+            Balance -= amt;
+            return Balance;
         }
     }
 }
--- BankAccountTests/AccountTests.cs
+++ BankAccountTests/AccountTests.cs
@@ -11,13 +11,25 @@ namespace BankAccount.Tests
     [TestClass()]
     public class AccountTests
     {
-        [TestMethod]
-        public void Deposit_APositiveAmmount_AddToBalance()
-        {
-            Account acc = new("J Dog");
-            acc.Deposit(100);
+        private Account acc;
 
-            Assert.AreEqual(100, acc.Balance);
+        [TestInitialize] // runs once before each test runs
+        public void CreateDefaultAccount()
+        {
+            acc = new Account("J Doe");
+        }
+
+        [TestMethod]
+        [DataRow(100)]
+        [DataRow(.01)]
+        [DataRow(1.00)]
+        [DataRow(1.999)]
+        [DataRow(9999.99)]
+        public void Deposit_APositiveAmmount_AddToBalance(double depositAmmount)
+        {
+            acc.Deposit(depositAmmount);
+
+            Assert.AreEqual(depositAmmount, acc.Balance);
         }
 
         [TestMethod]
@@ -26,7 +38,6 @@ namespace BankAccount.Tests
             // AAA - Arrange Act Assert
 
             // Arrange - set up variables
-            Account acc = new("J. Doe");
             double depositAmmount = 100;
             double expectedReturn = 100;
 
@@ -35,6 +46,40 @@ namespace BankAccount.Tests
 
             // Assert
             Assert.AreEqual(expectedReturn, returnValue);
+        }
+
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        public void Deposit_ZeroOrLess_ThrowsArgumentException(double invalidDepositAmount)
+        {
+            // Arrange
+            // Nothing to add here
+
+            // Assert => Act
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => acc.Deposit(invalidDepositAmount));
+        }
+
+        // Withdrawing a positive amount - returns updated balance
+        // Withdrawing 0 - Throws ArgumentOutOfRange exception
+        // Withdrawing negative amount - Throws ArgumentOutOfRange eception
+        // Withdrawing more than balance - ArgumentException
+        [TestMethod]
+        public void Withdraw_APositiveAmount_DecreasesBalance()
+        {
+            // Arrange
+            double initialDeposit = 100;
+            double withdrawAmount = 50;
+            double expectedBalance = initialDeposit - withdrawAmount;
+
+            // Act
+            acc.Deposit(initialDeposit);
+            acc.Withdraw(withdrawAmount);
+
+            double actualBalance = acc.Balance;
+
+            // Assert
+            Assert.AreEqual(expectedBalance, actualBalance);
         }
     }
 }
